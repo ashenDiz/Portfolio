23a710c0cd3619a37474705dc8581c7cc48f4c7a
ashenDiz <it21270642@my.sliit.lk>
2026-02-01 13:24:20 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,276 @@
+// src\constants\index.js
+export const myProjects = [
+  {
+    id: 1,
+    title: "Sri Lankan Tourism Website",
+    description:
+      "A modern tourism platform designed to showcase Sri Lanka as a premier travel destination, featuring the island's diverse attractions from pristine beaches and lush tea plantations to ancient cultural sites and wildlife sanctuaries",
+    subDescription: [
+      "The design employs a vibrant color palette inspired by Sri Lanka's natural beauty—ocean blues, sunset oranges, and tropical greens. Key features include an interactive destinations gallery, curated experience categories, and a comprehensive trip planning system covering popular locations like Ella, Galle, Kandy, and Yala National Park.",
+      "Built in Figma with modern design principles, the responsive interface ensures seamless navigation across all devices while balancing aesthetic appeal with functionality through smooth animations and clear call-to-action elements.",
+    ],
+    href: "https://www.figma.com/make/AwiHMfbl9BkszB0hdISgOa/Sri-Lankan-Tourism-Homepage?fullscreen=1&t=ido2ynBB0ZtUH7hh-1",
+    logo: "",
+    image: "/assets/projects/Sri Lankan Tourism Homepage.png",
+    tags: [
+      {
+        id: 1,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 2,
+        name: ".Net",
+        path: "/assets/logos/dotnet.svg",
+      },
+      {
+        id: 3,
+        name: "Ef Core",
+        path: "/assets/logos/efcore.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "UI/UX Redesign Proposal for Sri Lanka Post Website",
+    description:
+      "A secure authentication and authorization system using Auth0 for seamless user management.",
+    subDescription: [
+      "Integrated Auth0 for authentication, supporting OAuth, JWT, and multi-factor authentication.",
+      "Implemented role-based access control (RBAC) for fine-grained user permissions.",
+    ],
+    href: "https://www.figma.com/proto/Q0wnswfbfFZKp0pRZuMHYO/SL-Post?page-id=0%3A1&node-id=1-4&p=f&viewport=-737%2C-576%2C0.45&t=x4UDG1JPvJygHfbH-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1%3A4",
+    logo: "",
+    image: "/assets/projects/SL Post.png",
+    tags: [
+      {
+        id: 1,
+        name: "Figma",
+        path: "/assets/logos/figma.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Survival Game Website Design",
+    description:
+      "A modern, interactive web application built with Blazor WebAssembly and .NET Core.",
+    subDescription: [
+      "Developed a fully interactive Single Page Application (SPA) using Blazor WebAssembly.",
+      "Implemented API interactions using .NET Core for a robust backend.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "Integrated SQLite for efficient client-side database storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/game website.png",
+    tags: [
+      {
+        id: 1,
+        name: "Blazor",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: ".NET Core",
+        path: "/assets/logos/dotnetcore.svg",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Nike Store Website - UI/UX Concept Design",
+    description:
+      "A lightweight C++ game engine designed for 2D and 3D game development.",
+    subDescription: [
+      "Built a powerful rendering engine using OpenGL and C++.",
+      "Developed a physics engine with collision detection and particle effects.",
+      "Implemented a scripting system for easy game customization.",
+      "Optimized performance with multi-threading and efficient memory management.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/nike store.png",
+    tags: [
+      {
+        id: 1,
+        name: "Figma",
+        path: "/assets/logos/figma.png",
+      }
+    ],
+  },
+  {
+    id: 5,
+    title: "Paging Web App",
+    description:
+      "A fully customizable WordPress theme optimized for performance and SEO.",
+    subDescription: [
+      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/paging.png",
+    tags: [
+      {
+        id: 1,
+        name: "Java Script",
+        path: "/assets/logos/js.png",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      }
+    ],
+  },
+  {
+    id: 6,
+    title: "Social Media / Visual Discovery App",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Built using Blazor WebAssembly for a seamless SPA experience.",
+      "Implemented video streaming with Azure Media Services.",
+      "Added a quiz system with dynamic question generation and real-time grading.",
+      "Integrated Stripe API for secure payment processing.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/photos app.png",
+    tags: [
+      {
+        id: 1,
+        name: "Figma",
+        path: "/assets/logos/figma.png",
+      }
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://api.whatsapp.com/send/?phone=%2B94786495906&text&type=phone_number&app_absent=0",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ashen-dissanayake-372985216",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ashendiz?igsh=OHB2NWoyNzNzdHdt&utm_source=qr",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  
+  {
+    title: "UI/UX Designer",
+    job: "Less Taxi Pvt Ltd",
+    date: "2025-Present",
+    contents: [
+      "🪐 Designed end-to-end UI/UX for the Less Taxi paging application, focusing on usability, accessibility, and modern design principles",
+      "🪐 Converted UI/UX designs into fully functional frontend interfaces",
+      "🪐 Collaborated with stakeholders to align design decisions with business and user needs",
+      "🪐 Ensured responsive design and smooth user interactions across devices"
+    ],
+  },
+ {
+    title: "Intern Software Engineer",
+    job: "Virtual System Pvt Ltd",
+    date: "2024-2025",
+    contents: [
+      "🪐Developed a full-stack web-based application tailored for small and medium enterprises (SMEs) to streamline inventory and sales management.",
+      "🪐 Designed and implemented core modules: user, customer, and product management (CRUD operations).",
+      "🪐 Built secure login system using JWT authentication and bcrypt password hashing (no registration).",
+      "🪐 Developed invoicing module with customer/product selection, quantity input, and automatic stock deduction.",
+      "🪐 Engineered responsive React frontend with structured navigation (main and sub menus).",
+      "🪐 Designed MySQL database schema with relational integrity and foreign key constraints.",
+    ],
+  },
+  // {
+  //   title: "Freelance Developer",
+  //   job: "Self-Employed",
+  //   date: "2025-Present",
+  //   contents: [
+  //     "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+  //     "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+  //   ],
+  // },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
